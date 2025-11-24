de54579eb28e3549cf27f6e8711f1a47e23c3ad7
justin <kinhanjustin@gmail.com>
2025-11-24 13:53:49 +0100

    Remove click handler for closing modal in Plza component

--- src/Plza.jsx
+++ src/Plza.jsx
@@ -70,7 +70,6 @@ export default function Plza() {
             {/* ---------------------- */}
             {selectedPokemon && (
                 <div
-                    onClick={closeModal}
                     className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
                 >
                     <div
