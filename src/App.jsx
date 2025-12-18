3ae9940769d6b8b4cf7e8eb391bdfea8a94de354
justin <kinhanjustin@gmail.com>
2025-12-18 15:58:59 +0100

    Revert "Refactor App component to integrate new Pokémon datasets, enhancing data handling and collection management"
    This reverts commit 01de42bbb361378d6649c3d1da1d35e8a49a35d4.

--- src/App.jsx
+++ src/App.jsx
@@ -6,20 +6,13 @@ import Sv from "./Components/Sv/Sv.jsx";
 import Collection from "./Components/Collection/Collection.jsx";
 
 // Data imports
-import plzaPokemonBase from "./data/PlzaData.js";
-import plzaMdPokemon from "./data/PlzaMdData.js";
-import svPokemonBase from "./data/SvData.js";
-import svTmPokemon from "./data/SvTmData.js";
-import svIdPokemon from "./data/SvIdData.js";
+import plzaPokemon from "./data/PlzaData.js";
+import svPokemon from "./data/SvData.js";
 import shinyDexPart1 from "./data/ShinyDexData.js";
 import shinyDexPart2 from "./data/ShinyDexData2.js";
 import shinyDexPart3 from "./data/ShinyDexData3.js";
 
 const pokemonList = [...shinyDexPart1, ...shinyDexPart2, ...shinyDexPart3];
-
-// Combineer de data hier (PLZA stijl), zodat de Collection prop de volledige lijst krijgt
-const combinedPlza = plzaPokemonBase.concat(plzaMdPokemon);
-const combinedSv = svPokemonBase.concat(svTmPokemon, svIdPokemon);
 
 const router = createBrowserRouter([{
         element: <Layout/>,
@@ -30,8 +23,8 @@ const router = createBrowserRouter([{
             {
                 path: "/collection",
                 element: <Collection
-                    plzaPokemon={combinedPlza}
-                    svPokemon={combinedSv}
+                    plzaPokemon={plzaPokemon}
+                    svPokemon={svPokemon}
                     pokemonList={pokemonList}
                 />
             }
