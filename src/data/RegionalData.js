1dc03309afaf1a83fa537d2cd765d5743c5b5b43
justin <kinhanjustin@gmail.com>
2025-12-21 14:47:23 +0100

    Add regionalPokemon data for Alolan, Galarian, Hisuian, and Paldean forms

---
+++ src/data/RegionalData.js
@@ -0,0 +1,68 @@
+const regionalPokemon = [
+    // --- ALOLAN FORMS ---
+    { id: 10091, name: "Alolan Rattata", region: "Alola" },
+    { id: 10092, name: "Alolan Raticate", region: "Alola" },
+    { id: 10100, name: "Alolan Raichu", region: "Alola" },
+    { id: 10094, name: "Alolan Sandshrew", region: "Alola" },
+    { id: 10095, name: "Alolan Sandslash", region: "Alola" },
+    { id: 10096, name: "Alolan Vulpix", region: "Alola" },
+    { id: 10097, name: "Alolan Ninetales", region: "Alola" },
+    { id: 10098, name: "Alolan Diglett", region: "Alola" },
+    { id: 10099, name: "Alolan Dugtrio", region: "Alola" },
+    { id: 10101, name: "Alolan Meowth", region: "Alola" },
+    { id: 10102, name: "Alolan Persian", region: "Alola" },
+    { id: 10103, name: "Alolan Geodude", region: "Alola" },
+    { id: 10104, name: "Alolan Graveler", region: "Alola" },
+    { id: 10105, name: "Alolan Golem", region: "Alola" },
+    { id: 10106, name: "Alolan Grimer", region: "Alola" },
+    { id: 10107, name: "Alolan Muk", region: "Alola" },
+    { id: 10108, name: "Alolan Exeggutor", region: "Alola" },
+    { id: 10109, name: "Alolan Marowak", region: "Alola" },
+
+    // --- GALARIAN FORMS ---
+    { id: 10161, name: "Galarian Meowth", region: "Galar" },
+    { id: 10162, name: "Galarian Ponyta", region: "Galar" },
+    { id: 10163, name: "Galarian Rapidash", region: "Galar" },
+    { id: 10164, name: "Galarian Slowpoke", region: "Galar" },
+    { id: 10165, name: "Galarian Slowbro", region: "Galar" },
+    { id: 10166, name: "Galarian Farfetch'd", region: "Galar" },
+    { id: 10167, name: "Galarian Weezing", region: "Galar" },
+    { id: 10168, name: "Galarian Mr. Mime", region: "Galar" },
+    { id: 10169, name: "Galarian Articuno", region: "Galar" },
+    { id: 10170, name: "Galarian Zapdos", region: "Galar" },
+    { id: 10171, name: "Galarian Moltres", region: "Galar" },
+    { id: 10172, name: "Galarian Slowking", region: "Galar" },
+    { id: 10173, name: "Galarian Corsola", region: "Galar" },
+    { id: 10174, name: "Galarian Zigzagoon", region: "Galar" },
+    { id: 10175, name: "Galarian Linoone", region: "Galar" },
+    { id: 10176, name: "Galarian Darumaka", region: "Galar" },
+    { id: 10177, name: "Galarian Darmanitan", region: "Galar" },
+    { id: 10178, name: "Galarian Yamask", region: "Galar" },
+    { id: 10179, name: "Galarian Stunfisk", region: "Galar" },
+
+    // --- HISUIAN FORMS ---
+    { id: 10229, name: "Hisuian Growlithe", region: "Hisui" },
+    { id: 10230, name: "Hisuian Arcanine", region: "Hisui" },
+    { id: 10231, name: "Hisuian Voltorb", region: "Hisui" },
+    { id: 10232, name: "Hisuian Electrode", region: "Hisui" },
+    { id: 10233, name: "Hisuian Typhlosion", region: "Hisui" },
+    { id: 10234, name: "Hisuian Qwilfish", region: "Hisui" },
+    { id: 10235, name: "Hisuian Sneasel", region: "Hisui" },
+    { id: 10236, name: "Hisuian Samurott", region: "Hisui" },
+    { id: 10237, name: "Hisuian Lilligant", region: "Hisui" },
+    { id: 10238, name: "Hisuian Zorua", region: "Hisui" },
+    { id: 10239, name: "Hisuian Zoroark", region: "Hisui" },
+    { id: 10240, name: "Hisuian Braviary", region: "Hisui" },
+    { id: 10241, name: "Hisuian Sliggoo", region: "Hisui" },
+    { id: 10242, name: "Hisuian Goodra", region: "Hisui" },
+    { id: 10243, name: "Hisuian Avalugg", region: "Hisui" },
+    { id: 10244, name: "Hisuian Decidueye", region: "Hisui" },
+
+    // --- PALDEAN FORMS ---
+    { id: 10250, name: "Paldean Wooper", region: "Paldea" },
+    { id: 10251, name: "Paldean Tauros (Combat)", region: "Paldea" },
+    { id: 10252, name: "Paldean Tauros (Blaze)", region: "Paldea" },
+    { id: 10253, name: "Paldean Tauros (Aqua)", region: "Paldea" }
+];
+
+export default regionalPokemon;
